fcde7aafe1539697b8bf189723ebb077d2ffaa0b
Duane Robertson <duane@duanerobertson.com>
2017-11-15 00:43:13 -0600

    Use nodebox for bear.

--- bear.lua
+++ bear.lua
@@ -8,15 +8,8 @@
 
 do
   local def1 = {
-    animation = {
-      punch = {start = 70, stop = 100},
-      run = {start = 105, stop = 135},
-      stand = {start = 0, stop = 15, noloop = true},
-      walk = {start = 35, stop = 65},
-    },
     attacks_player = 10,
     armor_class = 6,
-    collisionbox = {-0.4, -0.01, -0.4, 0.4, 1, 0.4},
     damage = 2,
     drops = {
       {name = 'mobs:meat_raw',},
@@ -25,11 +18,22 @@ do
     environment = {'default:dirt_with_grass'},
     hit_dice = 6,
     name = 'bear',
-    mesh = "kpg_bear.x",
-    size = 2,
-    textures = {
-      {"kpg_bear.png"},
+    nodebox = {
+			{-0.25, -0.375, -0.5, 0.25, 0.125, 0.1875}, -- NodeBox1
+			{-0.25, -0.5, -0.5, -0.0625, -0.375, -0.3125}, -- NodeBox2
+			{0.0625, -0.5, -0.5, 0.25, -0.375, -0.3125}, -- NodeBox3
+			{0.0625, -0.5, 0, 0.25, -0.375, 0.1875}, -- NodeBox4
+			{0.0625, -0.5, 0, 0.25, -0.4375, 0.25}, -- NodeBox5
+			{-0.25, -0.5, 0, -0.0625, -0.4375, 0.25}, -- NodeBox6
+			{-0.25, -0.5, 0, -0.0625, -0.375, 0.1875}, -- NodeBox7
+			{-0.25, -0.5, -0.5, -0.0625, -0.4375, -0.25}, -- NodeBox8
+			{0.0625, -0.5, -0.5, 0.25, -0.4375, -0.25}, -- NodeBox9
+			{-0.155, -0.0625, 0.1875, 0.155, 0.1875, 0.375}, -- NodeBox10
+			{-0.0925, -0.0625, 0.375, 0.0925, 0.0625, 0.5}, -- NodeBox11
+			{-0.125, 0.1875, 0.25, -0.0625, 0.25, 0.3125}, -- NodeBox12
+			{0.0625, 0.1875, 0.25, 0.125, 0.25, 0.3125}, -- NodeBox13
     },
+    size = 2,
   }
 
   local def2 = table.copy(def1)
@@ -38,16 +42,12 @@ do
   def2.hit_dice = 8
   def2.name = 'kodiak'
   def2.size = 2.5
-  def2.textures = { {"polar_bear.png"}, }
-  def2.vision = 20
 
   local def3 = table.copy(def1)
   def3.environment = {'default:stone'}
   def3.glow = 2
-  def3.name = 'cave_bear'
+  def3.name = 'bear_cave'
   def3.size = 1
-  def3.textures = { {"cave_bear.png"}, }
-  def3.vision = 20
 
   nmobs.register_mob(def1)
   nmobs.register_mob(def2)
